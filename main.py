d9f70bfa767d7d2081f66d9a6ea2f01a83b3d970
Hkeri <128484996+Hkeri@users.noreply.github.com>
2025-04-04 16:04:28 +0400

    Update main.py

--- main.py
+++ main.py
@@ -63,109 +63,109 @@ def append_to_file(text):
 if __name__ == "__main__":
     while True:
       question = input("Enter Your Question: ")
-      cmd = groq('''if the query is asking about closing an application or website, respond "?!close (the application or website and make sure the website is in www and the .com also the application must be in its short form to run it in the Run Window like if the query is saying to close word, you must respond 'close winword'").
-                    if the query is asking about opening an application or website, respond "?!open (the application or website and make sure the website is in www and the .com also the application must be in its short form to run it in the Run Window like if the query is saying to open word, you must respond 'open winword'")
-                    if the query is asking about searching in youtube, then you must respond "?!youtube search (the youtube search)"
-                    if the query is asking about the battery, respond "?!battery"
-                    if the user is asking the internet speed, respond "?!internet speed"
-                    if the user is asking to lower their brightness of the screen, respond "?!dim light"
-                    if the user is asking to send in an email, respond "?!send email"
-                    if the user is asking to change the wallpaper, respond "?!change wallpaper"
-                    if the user is asking to display the top cpu and memory usage processes, respond "?!top processes"
-                    if the user is asking to create a file, respond "?!create file"
-                    if the user is asking to clean temporary files, respond "?!clean temporary files"
-                    if the user is asking to download images from the internet, respond "?!download image"
-                    if the user is asking to transcribe the audio, respond "?!transcribe audio"
-                    if the user is asking to organize files, respond "?!organize files"
-                    if the user is asking to read a pdf, respond "?!read pdf"
-                    if the user is asking to play music, respond "?!play music"
-                    if the user is asking to do an ocr, respond "?!ocr"
-                    if the user is not asking for anything in this list then respond "!none"
-                    if the user is asking for a whatsapp message, respond "?!whatsapp"
-                    if the user is asking for downloading a youtube video, respond "?!youtube download"
-                    if the user is asking to make a qrcode, respond "?!qrcode"
-                    if the user is asking to shutdown the computer, respond "?!shutdown"
-                    if the user is asking to restart the computer, respond "?!restart"
-                    if the user is asking to take a screenshot, respond "?!screenshot"
-                    if the user is asking to start a google meet, respond "?!google meeting"''', question)
+      cmd = groq('''if the query is asking about closing an application or website, respond "^close (the application or website and make sure the website is in www and the .com also the application must be in its short form to run it in the Run Window like if the query is saying to close word, you must respond 'close winword'").
+                    if the query is asking about opening an application or website, respond "^open (the application or website and make sure the website is in www and the .com also the application must be in its short form to run it in the Run Window like if the query is saying to open word, you must respond 'open winword'")
+                    if the query is asking about searching in youtube, then you must respond "^youtube search (the youtube search)"
+                    if the query is asking about the battery, respond "^battery"
+                    if the user is asking the internet speed, respond "^internet speed"
+                    if the user is asking to lower their brightness of the screen, respond "^dim light"
+                    if the user is asking to send in an email, respond "^send email"
+                    if the user is asking to change the wallpaper, respond "^change wallpaper"
+                    if the user is asking to display the top cpu and memory usage processes, respond "^top processes"
+                    if the user is asking to create a file, respond "^create file"
+                    if the user is asking to clean temporary files, respond "^clean temporary files"
+                    if the user is asking to download images from the internet, respond "^download image"
+                    if the user is asking to transcribe the audio, respond "^transcribe audio"
+                    if the user is asking to organize files, respond "^organize files"
+                    if the user is asking to read a pdf, respond "^read pdf"
+                    if the user is asking to play music, respond "^play music"
+                    if the user is asking to do an ocr, respond "^ocr"
+                    if the user is not asking for anything in this list then respond "^none"
+                    if the user is asking for a whatsapp message, respond "^whatsapp"
+                    if the user is asking for downloading a youtube video, respond "^youtube download"
+                    if the user is asking to make a qrcode, respond "^qrcode"
+                    if the user is asking to shutdown the computer, respond "^shutdown"
+                    if the user is asking to restart the computer, respond "^restart"
+                    if the user is asking to take a screenshot, respond "^screenshot"
+                    if the user is asking to start a google meet, respond "^google meeting"''', question)
       append_to_file(question)
 
-      if re.search("?!close", cmd):
-        cmd = cmd.replace("?!close", "")
+      if re.search(r"^close", cmd):
+        cmd = cmd.replace(r"^close", "")
         closeappweb(cmd)
       
-      if re.search("?!open", cmd):
-        cmd = cmd.replace("?!open", "")
+      if re.search(r"^open", cmd):
+        cmd = cmd.replace(r"^open", "")
         openappweb(cmd)
       
-      if re.search("?!youtube search", cmd):
-        cmd = cmd.replace("?!youtube search", "")
+      if re.search(r"^youtube search", cmd):
+        cmd = cmd.replace(r"^youtube search", "")
         yt_search(cmd)
     
-      if re.search("?!battery", cmd):
+      if re.search(r"^battery", cmd):
        smart_battery()
     
-      if re.search("?!internet speed", cmd):
+      if re.search(r"^internet speed", cmd):
         internet_speed()
     
-      if re.search("?!dim light", cmd):
+      if re.search(r"^dim light", cmd):
         dim_light()
         
-      if re.search("?!send email", cmd):
+      if re.search(r"^send email", cmd):
         email = input("Please type in the sender's email: ")
         msg = input("Please type in the message: ")
         send_email(msg, email)
     
-      if re.search("?!change wallpaper", cmd):
+      if re.search(r"^change wallpaper", cmd):
         image_path = input("Please type in the full Image Path: ")
         change_wallpaper(image_path)
     
-      if re.search("?!top processes", cmd):
+      if re.search(r"^top processes", cmd):
         display_top_processes()
     
-      if re.search("?!create file", cmd):
+      if re.search(r"^create file", cmd):
         extension = input("Please type in the file extension: ")
         create_file(extension)
     
-      if re.search("?!clean temporary files", cmd):
+      if re.search(r"^clean temporary files", cmd):
         clean_temp()
     
-      if re.search("?!download image", cmd):
+      if re.search(r"^download image", cmd):
         image_url = input("Please type in the image url: ")
         download_images(image_url)
 
-      if re.search("?!transcribe audio", cmd):
+      if re.search(r"^transcribe audio", cmd):
         audio_path = input("Please type in the Audio File Path: ")
         transcribe_audio(audio_path)
 
-      if re.search("?!organize files", cmd):
+      if re.search(r"^organize files", cmd):
         dir_ = input("Please type in the full form of the Directory Path: ")
         file_organizer(dir_)
     
-      if re.search("?!read pdf", cmd):
+      if re.search(r"^read pdf", cmd):
         pdf = input("Please type in the PDF Path: ")
         read_pdf(pdf)
     
-      if re.search("?!play music", cmd):
+      if re.search(r"^play music", cmd):
         song = input("Please type in the song name: ")
         playMusic(song)
     
-      if re.search("?!ocr", cmd):
+      if re.search(r"^ocr", cmd):
         image_ = input("Please type in the image path: ")
         ocr(image_)
     
-      if re.search("?!whatsapp", cmd):
+      if re.search(r"^whatsapp", cmd):
         whatsapp_send()
     
-      if re.search("?!youtube download", cmd):
+      if re.search(r"^youtube download", cmd):
         url = input("Please type in the youtube url: ")
         ytDownloader(url)
         
-      if re.search("?!qrcode", cmd):
+      if re.search(r"^qrcode", cmd):
         qr_url = input("Please type in the url: ")
         qrCodeGenerator(qr_url)
 
-      if re.search("?!screenshot", cmd):
+      if re.search(r"^screenshot", cmd):
         number = 0
         print("Sure Sir, I will give you 5 seconds to show you want to Screenshot")
         time.sleep(5)
@@ -174,7 +174,7 @@ if __name__ == "__main__":
         ss.save(f"screenshot_{number}.png")
         print("I Have made A screenshot sir")
 
-      if re.search("?!shutdown", cmd):
+      if re.search(r"^shutdown", cmd):
         if platform.system() == "Windows":
             os.system("shutdown /s /t 0")
         if platform.system() == "Linux":
@@ -184,7 +184,7 @@ if __name__ == "__main__":
         else:
             print("Unsupported OS")
 
-      if re.search("?!restart", cmd):
+      if re.search(r"^restart", cmd):
         if platform.system() == "Windows":
             os.system("shutdown /r /t 0")
         if platform.system() == "Linux":
@@ -194,11 +194,11 @@ if __name__ == "__main__":
         else:
             print("Unsupported OS")
     
-      if re.search("?!google meeting", cmd):
+      if re.search(r"^google meeting", cmd):
         os.system("start chrome https://meet.new")
     
-      if re.search("!none", cmd):
-        print(groq(question))
+      if re.search(r"^none", cmd):
+        print(groq("Give a short detailed answer", question))
 
       if re.search("exit", cmd):
         print("Exiting...")
